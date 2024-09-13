2dc343f3814022c7f2cce9c36cee350bd1be4c08
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-09-13 06:43:57 -0700

    added a new file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 5
+sum = num1+ num2
+subtraction = num1 - num2 
+multiplication = num1 * num2 
+print("addition of " , num1 , "and" , num2 , "is" , sum)
+print("subtraction of " , num1 , "and" , num2 , "is" , subtraction)
+print("multiplication of " , num1 , "and" , num2 , "is" , multiplication)
