ec517521ecde384b531dca71a22cb28d4af45c7a
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-09-13 06:50:47 -0700

    made changes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-num1 = 10
-num2 = 5
-sum = num1+ num2
-subtraction = num1 - num2 
-multiplication = num1 * num2 
-print("addition of " , num1 , "and" , num2 , "is" , sum)
-print("subtraction of " , num1 , "and" , num2 , "is" , subtraction)
-print("multiplication of " , num1 , "and" , num2 , "is" , multiplication)
+number1 = 10
+number2 = 5
+sum = number1+ number2
+subtraction = number1 - number2 
+multiplication = number1 * number2 
+print("addition of " , number1 , "and" , number2 , "is" , sum)
+print("subtraction of " , number1 , "and" , number2 , "is" , subtraction)
+print("multiplication of " , number1 , "and" , number2 , "is" , multiplication)
