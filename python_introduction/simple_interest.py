902e25e63af05b3e789c85d74ec9f9868737d6c7
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-09-13 08:15:02 -0700

    updated

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,4 +2,4 @@ principal = int(1000)
 rate = float(0.05)
 time = int(3)
 interest = principal * rate * time
-print("the simple interest is :" , interest)
+print("the simple interest is:" , interest)
