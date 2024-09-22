836cab9dc455e7d89df2a2df0fdbc46668f8c4c2
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-09-22 04:12:49 -0700

    updated

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,9 +1,8 @@
 num = int(input("Enter the size of the pattern:"))
 row = 0
-for i in range(0,num):
-    for j in range(0,num):
-        print("*",end =' ')
-    print()   
+while row<num:
+    print("*" * num) 
+    row +=1 
 
 
     
