30c360363a4d1e62aaea2ca240d11505b2c0bb5c
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-10-03 08:11:53 -0700

    updated

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,18 @@
 class BankAccount:
-    def __init__(self,account_balance) :
-        account_balance= 0
+    def __init__(self,account_balance=0) :
+        self.account_balance= account_balance
     def deposit(self,amount) : 
-        self.amount += account_balance
+        self.account_balance += amount
+         
+        
     def withdraw(self,amount) :
-        self.amount -= account_balance
-        if account_balance>amount:
+        if self.account_balance >= amount:
+            self.account_balance -= amount 
             return True
         else:
             return False
+ 
+
 
     def display_balance(self) :
-        print(f"Current Balance: ${amount}")
+        print(f"Current Balance: ${self.account_balance}") 
