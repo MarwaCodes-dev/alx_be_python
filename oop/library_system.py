046dc8a9ea1981b356cabd861bfe10ed29c78264
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-10-11 05:04:22 -0700

    updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,9 +1,3 @@
-#Composition - Library:
-
-#Attributes: books (a list to store instances of Book, EBook, and PrintBook).
-#Methods:
-#add_book(self, book): Adds a Book, EBook, or PrintBook instance to the library.
-#list_books(self): Prints details of each book in the library.
 class Book:
     def __init__(self,title,author):
         self.title=title
@@ -16,6 +10,21 @@ class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count= page_count    
-books=Book()
+class Library:
+    def __init__(self):
+        self.books =[]
+    def add_book(self,book):
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
+            if isinstance(book,EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
+            elif isinstance(book,PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}") 
+            else:
+                print(f"Book: {book.title} by {book.author}")       
+
+
+    
     
         
