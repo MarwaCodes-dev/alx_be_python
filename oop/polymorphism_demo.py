b9a7a5b6f4345e3705982389083900758cf0985c
Marwa Saeed <marwa.software.engineer@gmail.com>
2024-10-13 06:58:52 -0700

    updated

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,15 @@
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("This method should be overridden by derived classes")
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length=length
+        self.width = width
+    def area(self):
+        return f"The area of the Rectangle is: {self.length*self.width}"
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius     
+    def area(self):
+        return f"The area of the Circle is: {(3.14)*(self.radius)**2}"       
